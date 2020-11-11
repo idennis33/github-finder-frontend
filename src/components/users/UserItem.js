00d1f1df9c6807ab77d7de06c379d37f6e3a0fb9
Isaac Dennis <idennisgsu3@gmail.com>
2020-11-11 15:21:26 -0500

    Removed Components and just declared the state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -3,15 +3,13 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-constructor (){
-    super(); 
-    this.state = {
+state = {
         id: 'id', 
         login: 'mojombo',
         avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
         html_url: 'https://github.com/mojombo'
     }
-}
+
 
     render() {
         return (
