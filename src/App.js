0da1da6445d90fea43c933c07f16a279a01fdba1
Isaac Dennis <idennisgsu3@gmail.com>
2020-11-11 15:19:47 -0500

    Added constructor to make a state for Useritem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
      <Navbar/>
+     <UserItem />
     </div>
   );
 }
