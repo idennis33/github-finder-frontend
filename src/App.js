d976c0157f712fb0f7b98e0b4a6ddc3a1c159c05
Isaac Dennis <idennisgsu3@gmail.com>
2020-11-12 11:53:28 -0500

    Made a user component which is import the useritem component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
      <Navbar/>
-     <UserItem />
+     <div className="container">
+        <Users />
+     </div>
+     
     </div>
   );
 }
